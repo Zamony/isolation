59b394c8bce36ba4231ad894628800e70e31181b
Nikita Mochalov <mochalov.n@yandex.ru>
2020-04-01 18:19:51 +0300

    Added a test checking that minimax is correct

---
+++ tests/test_ai.py
@@ -0,0 +1,46 @@
+import random
+import unittest
+from unittest import mock
+
+from isolation import Player, RobotControlledPlayer, run_game, Winner
+
+
+class RandomControlledPlayer(Player):
+
+    def __init__(self):
+        self.rx = 0
+        self.ry = 0
+
+    def get_move(self, board):
+        x, y = board.get_ann_pos()
+        i = j = None
+        while True:
+            i = random.randint(x-1, x+1)
+            j = random.randint(y-1, y+1)
+            if board.move_ann(i, j):
+                break
+        for p in range(board.size()):
+            for s in range(board.size()):
+                if board.can_be_removed(p, s):
+                    self.rx = p
+                    self.ry = s
+                    return i, j
+
+    def get_remove(self, board):
+        return self.rx, self.ry
+
+
+class TestAI(unittest.TestCase):
+
+    def test_ai(self):
+        nvictory = 0
+        total = 10
+        for _ in range(total):
+            player_a = RandomControlledPlayer()
+            player_b = RobotControlledPlayer()
+            winner = run_game(mock.Mock(), player_a, player_b)
+            if winner == Winner.player_b:
+                nvictory += 1
+
+        print(nvictory)
+        self.assertTrue(nvictory > total//2)
