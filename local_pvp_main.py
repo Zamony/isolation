73005793d4ca3712b8431ad34be0b3694b658c9e
blazzen <pavel.zhukov.official@gmail.com>
2020-04-23 02:06:57 +0300

    Resolve merge conflicts

--- local_pvp_main.py
+++ local_pvp_main.py
@@ -1,7 +1,7 @@
 import isolation
 
 if __name__ == "__main__":
-    player_a = isolation.UserControlledPlayer(isolation.TUI.ANN_ICON)
-    player_b = isolation.UserControlledPlayer(isolation.TUI.BOB_ICON)
     ui = isolation.TUI()
+    player_a = isolation.UserControlledPlayer(ui, isolation.TUI.ANN_ICON)
+    player_b = isolation.UserControlledPlayer(ui, isolation.TUI.BOB_ICON)
     isolation.run_game(ui, player_a, player_b)
--- online_pvp_main.py
+++ online_pvp_main.py
@@ -10,32 +10,33 @@ available_commands = [HOST_COMMAND, JOIN_COMMAND]
 
 
 def get_help_str(commands):
-    return " or ".join(commands) + "?\n"
+    return " or ".join(commands) + "?"
 
 
 if __name__ == "__main__":
     help_str = get_help_str(available_commands)
     command = ""
     while command not in available_commands:
-        command = input(help_str).strip()
+        command = input("\n" + help_str + "\n").strip()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        ui = isolation.TUI()
+
         if command == HOST_COMMAND:
             port = int(input("Enter the desired port number: ").strip())
 
             connection_utils.start_server_on_port(server_socket, port)
             client_socket = connection_utils.blocking_wait_for_player(server_socket)
 
-            player_a = isolation.LocalUserControlledPlayer(isolation.TUI.ANN_ICON, client_socket)
-            player_b = isolation.RemoteUserControlledPlayer(isolation.TUI.BOB_ICON, client_socket)
+            player_a = isolation.LocalUserControlledPlayer(ui, isolation.TUI.ANN_ICON, client_socket)
+            player_b = isolation.RemoteUserControlledPlayer(ui, isolation.TUI.BOB_ICON, client_socket)
         elif command == JOIN_COMMAND:
             host = input(connection_utils.HOST_PROMPT).strip()
             port = int(input(connection_utils.PORT_PROMPT).strip())
 
             server_socket.connect((host, int(port)))
 
-            player_a = isolation.RemoteUserControlledPlayer(isolation.TUI.ANN_ICON, server_socket)
-            player_b = isolation.LocalUserControlledPlayer(isolation.TUI.BOB_ICON, server_socket)
+            player_a = isolation.RemoteUserControlledPlayer(ui, isolation.TUI.ANN_ICON, server_socket)
+            player_b = isolation.LocalUserControlledPlayer(ui, isolation.TUI.BOB_ICON, server_socket)
 
-        ui = isolation.TUI()
         isolation.run_game(ui, player_a, player_b)
